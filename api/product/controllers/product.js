2e761cc36e33de1530c9f2c5ac869da909b03f5f
Thaihm <thaidd7b@gmail.com>
2021-06-03 18:28:29 +0700

    sua search query

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -7,9 +7,23 @@
 
 module.exports = {
   async search(ctx) {
+    if (ctx.query.tenSP) {
+      let x = await strapi
+        .query("product")
+        .search({ _q: ctx.query.tenSP, _limit: 10 });
+
+      return x;
+    } else {
+      return "thiếu query tenSP và phanLoai";
+    }
+  },
+
+  async searchByPhanLoai(ctx) {
+    let y = ctx.query.phanLoai;
+    console.log(y);
     let x = await strapi
       .query("product")
-      .search({ _q: ctx.query.tenSP, _limit: 10 });
+      .search({ _q: ctx.query.tenSP, phanLoai: y, _limit: 10 });
 
     return x;
   },
